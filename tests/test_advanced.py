a467bcbec2a6cf0d170a9bfbd271df474b1bfacf
incognitoRepo <incognitoRepo@outlook.com>
2019-10-23 14:52:44 -0700

    startup $(date)

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -9,7 +9,7 @@ class AdvancedTestSuite(unittest.TestCase):
   """Advanced test cases."""
 
   def test_thoughts(self):
-    self.assertIsNone(sample.hmm())
+    self.assertIsNone(hdlogger.hmm())
 
 
 if __name__ == '__main__':
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,9 +11,6 @@ class BasicTestSuite(unittest.TestCase):
   def test_absolute_truth_and_meaning(self):
     assert True
 
-  def test_hmm(self):
-
-
 
 if __name__ == '__main__':
     unittest.main()

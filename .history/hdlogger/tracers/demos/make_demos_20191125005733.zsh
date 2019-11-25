71265a64d0fd7c9ee96c7122182c6bb23e952274
incognitoRepo <incognitoRepo@outlook.com>
2019-11-25 01:05:26 -0800

    startup Mon Nov 25 01:05:26 PST 2019

--- .history/hdlogger/tracers/demos/make_demos_20191125005733.zsh
+++ .history/hdlogger/tracers/demos/make_demos_20191125005733.zsh
@@ -8,7 +8,7 @@ tracers=(
 
 
 path=(
-  /Users/alberthan/VSCodeProjects/vytd/src/youtube-dl
+  /Users/alberthan/VSCodeProjects/vytd/bin
   $path
 )
 python3 -m trace --count --trace --module -C . youtube_dl https://www.youtube.com/watch\?v\=f2exP40AZ6c
